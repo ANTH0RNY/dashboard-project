10da9856a7abd2ac075be32f0680d7eca2826585
ANTH0RNY <101851338+ANTH0RNY@users.noreply.github.com>
2023-06-22 10:02:15 +0300

    Done with pie chart for the main section

---
+++ main.js
@@ -0,0 +1,87 @@
+const xValues = ['Electronics', 'Food', 'Furniture', 'Cars and accessories', 'Others']
+const yValues = [60, 20, 30, 10, 25]
+const barColors = [
+    "rgba(1,22,39,.3)",
+    "rgba(255, 51, 102, .3",
+    "rgba(46, 196, 182, .3)",
+    "rgba(32, 164, 243, .3)",
+    "rgba(30,113, 69, .3)",
+    "rgba(153,93,129,.3)"
+]
+
+const borderColor = [
+    "rgb(1,22,39)",
+    "rgb(255,51,102)",
+    "rgb(46, 196,182)",
+    "rgb(32,164,243)",
+    "rgb(30,113,69)",
+    "rgb(153,93,129)"
+]
+new Chart("pie-1", {
+    type: "pie",
+    data: {
+        labels: xValues,
+        datasets: [{
+            backgroundColor: barColors,
+            borderColor: borderColor,
+            data: yValues
+        }]
+    },
+    options: {
+        title: {
+            display: true,
+            text: "Leading Products"
+        }
+    }
+});
+
+const xVal1 = ['East Products', 'Containers', 'Dry port', 'Gen Warehouse', 'Others']
+const yVal1 = [50, 40, 30, 25, 30]
+
+new Chart("pie-2", {
+    type: "pie",
+    data: {
+        labels: xVal1,
+        datasets: [{
+            backgroundColor: barColors,
+            borderColor: borderColor,
+            data: yVal1
+        }]
+    },
+    options: {
+        title: {
+            display: true,
+            text: "Main Warehouses"
+        }
+    }
+});
+
+const xVal2 = ['North America', 'Europe', 'Asia', 'Australia', 'Africa', 'South America']
+const yVal2 = [60, 40, 50, 30, 25, 20]
+
+// const barColors1=[
+//     "#011627",
+//     "#ff3366",
+//     "#2ec4b6",
+//     "#20A4f3",
+//     "#1e7145",
+//     "#f6f740"
+//   ]
+
+new Chart("pie-3", {
+    type: "pie",
+    data: {
+        labels: xVal2,
+        datasets: [{
+            backgroundColor: barColors,
+            borderColor: borderColor,
+            data: yVal2
+        }]
+    },
+    options: {
+        title: {
+            display: true,
+            text: "Markets"
+        }
+    }
+});
